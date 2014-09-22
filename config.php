e82985906c1f829ca738d61779640d21252afbe6
Ricardo <ricardochampa@gmail.com>
2014-09-23 00:46:17 +0200

    openshift

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
-define('HOSTNAME', 'localhost');  
-define('USER_DB', 'root');  
-define('PASS_DB', '');  
-define('DATABASE', 'a7432442_groupon');  
+//define('HOSTNAME', 'localhost');  
+//define('USER_DB', 'root');  
+//define('PASS_DB', '');  
+//define('DATABASE', 'a7432442_groupon');  
 
 define('HOSTNAME', getenv('OPENSHIFT_MYSQL_DB_HOST'). ':' . getenv('OPENSHIFT_MYSQL_DB_PORT'));  
 define('USER_DB', getenv('OPENSHIFT_MYSQL_DB_USERNAME'));  
