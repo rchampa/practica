8d33581f06bdddc11178afcf8db49d01038f7a6a
Ricardo <ricardochampa@gmail.com>
2014-09-23 00:22:27 +0200

    config para openshift

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ define('USER_DB', 'root');
 define('PASS_DB', '');  
 define('DATABASE', 'a7432442_groupon');  
 
-define('HOSTNAME', 'localhost');  
-define('USER_DB', 'root');  
-define('PASS_DB', '');  
-define('DATABASE', 'a7432442_groupon');  
+define('HOSTNAME', getenv('OPENSHIFT_MYSQL_DB_HOST'). ':' . getenv('OPENSHIFT_MYSQL_DB_PORT'));  
+define('USER_DB', getenv('OPENSHIFT_MYSQL_DB_USERNAME'));  
+define('PASS_DB', getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));  
+define('DATABASE', getenv('OPENSHIFT_APP_NAME'));  
